9e80b286a19dfc391f39b9d0b8704d233953f6b0
13507enautica <13507@enautica.pt>
2026-03-12 12:23:59 +0000

    exercicio 1 feito

--- src/main/kotlin/cm/exer_1/exer_1.kt
+++ src/main/kotlin/cm/exer_1/exer_1.kt
@@ -1,16 +1,12 @@
 package org.example.cm.exer_1
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val exemplo1 = IntArray(50) {(it+1)*(it+1)}
+    val exemplo2 = (1..50).map {it*it}
+    val exemplo3 = Array(50) {(it+1)*(it+1)}
+    println(exemplo1.joinToString(" "))
+    println(exemplo2.joinToString(" "))
+    println(exemplo3.joinToString(" "))
+
 }
