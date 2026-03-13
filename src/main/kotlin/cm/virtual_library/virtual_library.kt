e3d1cd4add5f51111a32ecf23286d613960e0979
13507enautica <13507@enautica.pt>
2026-03-13 09:05:26 +0000

    inicio de programacao da biblioteca virtual

--- src/main/kotlin/cm/virtual_library/virtual_library.kt
+++ src/main/kotlin/cm/virtual_library/virtual_library.kt
@@ -1,16 +1,17 @@
 package org.example.cm.virtual_library
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class Book(val title:String, val author:String, val publicationYear:Int, var availableCopies:Int) {
+    init { // verificacao de erros possiveis
+        require(availableCopies>0) {"Número de cópias tem que ser positivo.."}
+        require(publicationYear>0) {"O ano terá que ser um valor positivo."}
+        require(publicationYear<2027) {"O livro não pode ser do futuro."}
     }
+    init { // print final
+        println("'$title' by $author has been added to the library. ")
+    }
+}
+
+
+fun main() {
+    val book1 = Book("the", "theguy", 2027, 2)
 }
