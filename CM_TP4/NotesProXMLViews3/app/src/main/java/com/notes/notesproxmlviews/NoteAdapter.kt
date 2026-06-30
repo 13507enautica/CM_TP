26292d1411dea01523a8173e5e9e86f4febbc795
13507enautica <13507@enautica.pt>
2026-06-30 16:40:54 +0100

    implementação notes app

---
+++ CM_TP4/NotesProXMLViews3/app/src/main/java/com/notes/notesproxmlviews/NoteAdapter.kt
@@ -0,0 +1,14 @@
+package com.notes.notesproxmlviews
+
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
+
+class NoteAdapter {
+
+    class NoteViewHolder {
+        var titleTextView : TextView?=null
+        var contentTextView : TextView?=null
+        var timestampTextView : TextView?=null
+
+    }
+}
